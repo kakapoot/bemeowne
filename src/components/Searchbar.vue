4467785ffed5720c7abbd48e74a3188c0749ad29
YJ <angyujuan.2001@gmail.com>
2023-02-20 03:49:46 +0800

    Implemented search by species

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,15 +1,22 @@
 <template>
-    <select onfocus='this.size=5;' onblur='this.size=1;' onchange='this.size=1; this.blur();'
-        class="form-select form-select-lg mb-5" aria-label="Default select example">
+    <select v-model="selected" @change="$emit('selectBreed', selected)" onfocus='this.size=5;' onblur='this.size=1;'
+        onchange='this.size=1; this.blur();' class="form-select form-select-lg mb-4" aria-label="Default select example">
         <option value="all" selected>All Species</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        <option v-for="breed in breeds" :value="breed.id" :key="breed.id">{{ breed.name }}</option>
     </select>
 </template>
 <script>
 export default {
     name: "Searchbar",
+    props: {
+        breeds: Array
+    },
+    data() {
+        return {
+            selected: "all"
+        }
+    },
+    emits: ["selectBreed"]
 }
 </script>
 <style></style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,14 @@
 <template>
-    <main class="d-flex flex-column align-items-center justify-content-center container">
-        <Searchbar />
+    <main class="d-flex flex-column align-items-center justify-content-center container mb-4">
+        <button type="button" @click="fetchSearchResult()" class="btn btn-lg btn-primary mb-4 text-light">Find More
+            Pawtential
+            Partners</button>
+        <Searchbar @select-breed="selectBreed" v-if="breeds" :breeds="breeds" />
         <div v-if="loading" class="spinner-border text-primary row" style="width: 5rem; height: 5rem;" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
         <div v-if="error" class="text-light fs-4">{{ error }}</div>
-        <div v-if="cats" class="row row-cols-md-2 row-cols-lg-3 g-5">
+        <div v-if="!loading && cats" class="row row-cols-md-2 row-cols-lg-3 g-5">
             <Card v-for="cat in cats" :key="cat.id" :id="cat.id" :imgSrc="cat.url" :breed="cat.breeds[0].name"
                 :country="cat.breeds[0].country_code" :description="cat.breeds[0].temperament" />
         </div>
@@ -22,21 +25,43 @@ export default {
     data() {
         return {
             breeds: null,
-            cats: null
+            cats: null,
+            selectedBreedId: "all"
         }
     },
     mixins: [FetchData],
     methods: {
+        fetchBreeds() {
+            this.fetchData("https://api.thecatapi.com/v1/breeds")
+                .then((fetchedData) => {
+                    this.breeds = fetchedData
+                })
+        },
+
         fetchSearchResult() {
-            this.fetchData("https://api.thecatapi.com/v1/images/search?has_breeds=true&order=RAND&page=0&limit=6")
+            let url = "https://api.thecatapi.com/v1/images/search?has_breeds=true&limit=6"
+            if (this.selectedBreedId !== "all") {
+                url = `https://api.thecatapi.com/v1/images/search?breed_ids=${this.selectedBreedId}&limit=6`
+            }
+
+            this.fetchData(url)
                 .then((fetchedData) => {
                     this.cats = fetchedData
                 })
+        },
+
+        selectBreed(id) {
+            this.selectedBreedId = id
         }
     },
     created() {
+        this.fetchBreeds()
         this.fetchSearchResult()
-    },
+    }, watch: {
+        selectedBreedId: function () {
+            this.fetchSearchResult()
+        }
+    }
 }
 </script>
 <style></style>
